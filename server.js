2dad01c726e79b218a2e38bbea14020f0955809e
Caio Thommen Cardoso <1100127915@edu.cbe.ab.ca>
2024-10-29 13:58:50 -0600

    started on guess

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ let activeSessions={}
 
 server.get('/newgame',(req, res) =>{
     let newID = uuid.v4()
+    let answer = req.query.answer
     let newGame = {
         wordToGuess: "apple",
         guesses:[],
@@ -20,6 +21,8 @@ server.get('/newgame',(req, res) =>{
         gameOver: false
         
     }
+    if(answer){
+        newGame.wordToGuess = answer}
     activeSessions[newID] = newGame
     res.status(201)
     res.send({sessionID: newID})
@@ -27,10 +30,32 @@ server.get('/newgame',(req, res) =>{
 
 server.get('/gamestate', (req, res) => {
     let gameState = activeSessions[req.query.sessionID]
-    
     res.status(200)
-    res.send(gameState)
+    res.send({gameState: gameState})
+    
+})
+
+
+server.post('/guess', (req, res) => {
+    let guess = req.body.guess.split("")
+    let sessionID = req.body.sessionID
+    let game = activeSessions[sessionID]
+    let answer = game.wordToGuess.split("")
+
+    for(i = 0; i > 5; i++){
+        
+    }
+
 })
 //Do not remove this line. This allows the test suite to start
 //multiple instances of your server on different ports
 module.exports = server;
+
+
+
+if(guess[i] == answer[i]){
+
+}else if(answer.includes(guess[i])){
+
+}
+else{}
